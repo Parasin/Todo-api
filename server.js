8736f24269d0e4f4c01b7a2fe06ac55ae212fe13
Evan Bechtol <evanbechtol@gmail.com>
2016-05-16 08:48:36 -0500

    Added underscore lib, refactored todos

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -13,17 +14,15 @@ app.use(bodyParser.json());
 
 /* Get individual todo */
 app.get('/todos/:id', function (req, res) {
-    var id = parseInt(req.params.id, 10);
-    todos.forEach(function (todo) {
-        if (todo.id === id) {
-            
-            res.json(todo);        
-        }
-        else {
-            // Do nothing
-        }
-    });
-    res.status(404).send();
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if (matchedTodo) {
+        res.json(matchedTodo)
+    }
+    else {
+        res.status(404).send();   
+    }
 });
 
 /* Get all todos */
@@ -32,10 +31,14 @@ app.get('/todos', function (req, res) {
 });
 
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
     
-    if (body) {
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+    else {
         body.id = todoNextId++;
+        body.description = body.description.trim();
         todos.push(body);
     }
     
