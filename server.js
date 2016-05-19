581b51fe1129c8d6d908bdf1f836f0873409c976
Evan Bechtol <evanclaybechtol@gmail.com>
2016-05-19 11:08:26 -0500

    Converted  GET /todos/? to sequelize

--- server.js
+++ server.js
@@ -21,7 +21,9 @@ app.get('/todos/:id', function (req, res) {
         if (!_.isNull(todo)) {
             res.json(todo);
         } else {
-            res.status(404).json({"error": 'No todo found with that id.'});
+            res.status(404).json({
+                "error": 'No todo found with that id.'
+            });
         }
 
     }).catch(function (err) {
@@ -31,27 +33,28 @@ app.get('/todos/:id', function (req, res) {
 
 /* Get all todos */
 app.get('/todos', function (req, res) {
-    var queryParams = req.query;
-    var filteredTodos = todos;
+    var query = req.query;
+    var where = {};
 
-    /* Filters todos based on the query param containing 'completed' */
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-        filteredTodos = _.where(filteredTodos, {
-            'completed': true
-        });
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-        filteredTodos = _.where(filteredTodos, {
-            'completed': false
-        });
+    if (query.hasOwnProperty('completed') && query.completed === 'true') {
+        where.completed = true;
+    } else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+        where.completed = false;
     }
 
-    if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
-        filteredTodos = _.filter(filteredTodos, function (todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1
-        });
+    if (query.hasOwnProperty('q') && query.q.length > 0) {
+        where.description = {
+            "$like": '%' + query.q + '%'
+        };
     }
 
-    res.json(filteredTodos);
+    db.todo.findAll({
+        "where": where
+    }).then(function (todos) {
+        res.json(todos);
+    }).catch(function (err) {
+        res.status(500).send(err);
+    });
 });
 
 /* POST Create new todo*/
