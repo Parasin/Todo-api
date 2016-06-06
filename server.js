279238e708a06e18fd9d286a2ac58dbed38d64f6
Evan Bechtol <evanbechtol@gmail.com>
2016-06-06 13:14:37 -0500

    Add user model and POST /users req

--- server.js
+++ server.js
@@ -124,6 +124,18 @@ app.put('/todos/:id', function (req, res) {
     });
 });
 
+
+/* POST users */
+app.post('/users', function (req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+    
+    db.user.create(body).then(function (user) {
+        res.json(user.toJSON());
+    }).catch(function (err) {
+        res.status(400).json(err);
+    });
+});
+
 // Sync the database
 db.sequelize.sync().then(function () {
     app.listen(PORT, function () {
